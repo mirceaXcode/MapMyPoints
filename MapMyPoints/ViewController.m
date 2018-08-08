267e34103cbf0c8777682cd12ae3ae9b9695686c
Mircea Popescu <mirceap@gmail.com>
2018-08-08 08:41:53 -0400

    Latest version with comments

--- MapMyPoints/ViewController.m
+++ MapMyPoints/ViewController.m
@@ -7,8 +7,19 @@
 //
 
 #import "ViewController.h"
+#import "MapKit/MapKit.h"
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@property (strong, nonatomic) MKPointAnnotation *gemaltoAnno;
+@property (strong, nonatomic) MKPointAnnotation *bigskybarAnno;
+@property (strong, nonatomic) MKPointAnnotation *nycpizzaAnno;
+@property (strong, nonatomic) MKPointAnnotation *currentAnno;
+
+@property (weak, nonatomic) IBOutlet UISwitch *switchField;
+
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -16,14 +27,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.locationManager = [[CLLocationManager alloc] init];
+    [self.locationManager.delegate self];
+    // Requesting user authorisation to allow the location + setting in info.plist -> NSLocationWhenInUseUsageDescription
+    [self.locationManager requestWhenInUseAuthorization];
+    
+    [self addAnnotations];
+    
+    
+}
+- (IBAction)gemaltoTapped:(id)sender {
+    [self centerMap:_gemaltoAnno];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)bigskyTapped:(id)sender {
+    [self centerMap:_bigskybarAnno];
 }
 
+- (IBAction)nycpizzaTapped:(id)sender {
+    [self centerMap:_nycpizzaAnno];
+}
+
+-(void) centerMap:(MKPointAnnotation *) centerPoint{
+    [self.mapView setCenterCoordinate:centerPoint.coordinate animated:YES];
+}
+
+- (IBAction)switchChanged:(id)sender {
+    if (self.switchField.isOn){
+        self.mapView.showsUserLocation = YES;
+        [self.locationManager startUpdatingLocation];
+    } else {
+        self.mapView.showsUserLocation = NO;
+        [self.locationManager stopUpdatingLocation];
+    }
+}
+
+-(void) locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations {
+    self.currentAnno.coordinate = locations.lastObject.coordinate;
+    
+    [self centerMap:self.currentAnno];
+}
+
+-(void) addAnnotations{
+    
+    self.gemaltoAnno = [[MKPointAnnotation alloc] init];
+    self.gemaltoAnno.coordinate = CLLocationCoordinate2DMake(30.388751,-97.7488101);
+    self.gemaltoAnno.title = @"Gemalto office in Austin,TX";
+    
+    self.bigskybarAnno = [[MKPointAnnotation alloc] init];
+    self.bigskybarAnno.coordinate = CLLocationCoordinate2DMake(45.2613924,-111.3194378);
+    self.bigskybarAnno.title = @"Broken Spoke Bar in Big Sky,MT";
+    
+    self.nycpizzaAnno = [[MKPointAnnotation alloc] init];
+    self.nycpizzaAnno.coordinate = CLLocationCoordinate2DMake(40.756389,-73.9964807);
+    self.nycpizzaAnno.title = @"Capizzi Pizza in NYC";
+    
+    self.currentAnno = [[MKPointAnnotation alloc] init];
+    self.currentAnno.coordinate = CLLocationCoordinate2DMake(0.0,0.0);
+    self.currentAnno.title = @"My Location";
+    
+    // Adding them to the map
+    [self.mapView addAnnotations:@[self.gemaltoAnno, self.bigskybarAnno, self.nycpizzaAnno]];
+}
 
 @end
